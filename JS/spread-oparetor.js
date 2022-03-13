4dbf06e82f2db575e7d71f0432250b587fd18957
mahadihasan2 <mahadihasan125643@gmail.com>
2022-03-13 19:18:52 +0600

    spread-oparetor

---
+++ JS/spread-oparetor.js
@@ -0,0 +1,15 @@
+const numbers = [68, 43, 32, 21]
+numbers.push(48)
+
+// console.log([...numbers])
+const arrayMax = Math.max(57, 43, 32, 21)
+console.log(arrayMax)
+
+
+
+// const max = Math.max(...numbers)
+// console.log(max)
+
+const newarray = [...numbers, 45]
+console.log(numbers)
+console.log(newarray)
